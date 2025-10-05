5cc6198b346bc09275b8775e37e7f61c48f55619
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2025-10-05 21:19:11 +0800

    test

--- projects/bl616/main.c
+++ projects/bl616/main.c
@@ -1,17 +1,169 @@
+/*
+ * @Author: Dyyt587 67887002+Dyyt587@users.noreply.github.com
+ * @Date: 2024-03-30 11:14:00
+ * @LastEditors: Dyyt587 67887002+Dyyt587@users.noreply.github.com
+ * @LastEditTime: 2025-10-05 20:55:27
+ * @FilePath: \CherryDAP\projects\bl616\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "board.h"
 #include "dap_main.h"
 #include "bflb_gpio.h"
 #include "usb2uart.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "mem.h"
+
+#include <lwip/tcpip.h>
+#include <lwip/sockets.h>
+#include <lwip/netdb.h>
+
+#include "bl_fw_api.h"
+#include "wifi_mgmr_ext.h"
+#include "wifi_mgmr.h"
+
+#include "bflb_irq.h"
+#include "bflb_uart.h"
+
+#include "bl616_glb.h"
+#include "rfparam_adapter.h"
+
+#include "board.h"
+#include "shell.h"
+
+#define DBG_TAG "MAIN"
+#include "log.h"
+
+#define WIFI_STACK_SIZE  (1536)
+#define TASK_PRIORITY_FW (16)
+#define TASK_PRIORITY_DAP (30)
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static struct bflb_device_s *uart0;
+
+static TaskHandle_t wifi_fw_task;
+static TaskHandle_t dap_fw_task;
+
+static wifi_conf_t conf = {
+    .country_code = "CN",
+};
+extern void shell_init_with_task(struct bflb_device_s *shell);
+
+void wifi_event_handler(uint32_t code)
+{
+    switch (code) {
+        case CODE_WIFI_ON_INIT_DONE: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_INIT_DONE\r\n", __func__);
+            wifi_mgmr_init(&conf);
+        } break;
+        case CODE_WIFI_ON_MGMR_DONE: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_MGMR_DONE\r\n", __func__);
+        } break;
+        case CODE_WIFI_ON_SCAN_DONE: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_SCAN_DONE\r\n", __func__);
+            wifi_mgmr_sta_scanlist();
+        } break;
+        case CODE_WIFI_ON_CONNECTED: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_CONNECTED\r\n", __func__);
+            void mm_sec_keydump();
+            mm_sec_keydump();
+        } break;
+        case CODE_WIFI_ON_GOT_IP: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_GOT_IP\r\n", __func__);
+            LOG_I("[SYS] Memory left is %d Bytes\r\n", kfree_size());
+        } break;
+        case CODE_WIFI_ON_DISCONNECT: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_DISCONNECT\r\n", __func__);
+        } break;
+        case CODE_WIFI_ON_AP_STARTED: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_AP_STARTED\r\n", __func__);
+        } break;
+        case CODE_WIFI_ON_AP_STOPPED: {
+            LOG_I("[APP] [EVT] %s, CODE_WIFI_ON_AP_STOPPED\r\n", __func__);
+        } break;
+        case CODE_WIFI_ON_AP_STA_ADD: {
+            LOG_I("[APP] [EVT] [AP] [ADD] %lld\r\n", xTaskGetTickCount());
+        } break;
+        case CODE_WIFI_ON_AP_STA_DEL: {
+            LOG_I("[APP] [EVT] [AP] [DEL] %lld\r\n", xTaskGetTickCount());
+        } break;
+        default: {
+            LOG_I("[APP] [EVT] Unknown code %u \r\n", code);
+        }
+    }
+}
+
+int wifi_start_firmware_task(void)
+{
+    LOG_I("Starting wifi ...\r\n");
+
+    /* enable wifi clock */
+
+    GLB_PER_Clock_UnGate(GLB_AHB_CLOCK_IP_WIFI_PHY | GLB_AHB_CLOCK_IP_WIFI_MAC_PHY | GLB_AHB_CLOCK_IP_WIFI_PLATFORM);
+    GLB_AHB_MCU_Software_Reset(GLB_AHB_MCU_SW_WIFI);
+
+    /* Enable wifi irq */
+
+    extern void interrupt0_handler(void);
+    bflb_irq_attach(WIFI_IRQn, (irq_callback)interrupt0_handler, NULL);
+    bflb_irq_enable(WIFI_IRQn);
+
+    xTaskCreate(wifi_main, (char *)"fw", WIFI_STACK_SIZE, NULL, TASK_PRIORITY_FW, &wifi_fw_task);
+
+    return 0;
+}
+
+void dap_main(void *param)
+{
+        chry_dap_init(0, 0x20072000);
+    while (1) {
+        chry_dap_handle();
+        chry_dap_usb2uart_handle();
+    }
+}
+
+
+int dap_start_firmware_task(void)
+{
+    LOG_I("Starting dap ...\r\n");
+
+
+    xTaskCreate(dap_main, (char *)"dap", WIFI_STACK_SIZE, NULL, TASK_PRIORITY_DAP, &dap_fw_task);
+
+    return 0;
+}
 int main(void)
 {
     board_init();
 
     uartx_preinit();
 
-    chry_dap_init(0, 0x20072000);
+
+    uart0 = bflb_device_get_by_name("uart0");
+    shell_init_with_task(uart0);
+
+    if (0 != rfparam_init(0, NULL, 0)) {
+        LOG_I("PHY RF init failed!\r\n");
+        return 0;
+    }
+
+    LOG_I("PHY RF init success!\r\n");
+
+    tcpip_init(NULL, NULL);
+    wifi_start_firmware_task();
+    dap_start_firmware_task();  
+    vTaskStartScheduler();
     while (1) {
-        chry_dap_handle();
-        chry_dap_usb2uart_handle();
+        // chry_dap_handle();
+        // chry_dap_usb2uart_handle();
     }
 }
